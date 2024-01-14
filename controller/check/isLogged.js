e26ee1da39a843c0f1faff5da5b268fe65bd015a
MattiaSemproli <mattia.semproli2@studio.unibo.it>
2024-01-14 14:32:40 +0100

    Modified redirect and load after logged user check

--- controller/check/isLogged.js
+++ controller/check/isLogged.js
@@ -1,7 +1,16 @@
+const body = document.querySelector("body");
+
 document.addEventListener("DOMContentLoaded", function() {
     if (sessionStorage.getItem("loggedUser") == null) {
-        setTimeout(function() {
+        /*setTimeout(function() {
             window.location.href = "../../view/auth/login.html";
-        }, 500);
+        }, 500);*/
+        let nav = document.createElement("nav");
+        nav.setAttribute("id", "nav-to-load");
+        body.appendChild(nav);
+        $(function() {
+            $("#nav-to-load").load("../../view/common/commonfunc.html #common-navbar");
+        });
+    } else {
     }
 });
