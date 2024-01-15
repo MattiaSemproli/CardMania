76a8028feb295b632c62ed93c4364a2bd6bbd6ed
MattiaSemproli <mattia.semproli2@studio.unibo.it>
2024-01-15 23:48:35 +0100

    Modified some stuff in html

--- controller/check/isLogged.js
+++ controller/check/isLogged.js
@@ -5,11 +5,8 @@ document.addEventListener("DOMContentLoaded", function() {
         /*setTimeout(function() {
             window.location.href = "../../view/auth/login.html";
         }, 500);*/
-        let nav = document.createElement("nav");
-        nav.setAttribute("id", "nav-to-load");
-        body.appendChild(nav);
         $(function() {
-            $("#nav-to-load").load("../../view/common/commonfunc.html #common-navbar");
+            $(".nav-to-load").load("../../view/common/commonfunc.html .common-navbar");
         });
     } else {
     }
